f6f74f95c1eb2a1d36ea4099ce920fb6bb368719
chinmaybhat992-create <chinmaybhat992@gmail.com>
2025-09-05 12:53:31 +0530

    Create LC_217.java

---
+++ LC_217.java
@@ -0,0 +1,21 @@
+/*CONTAINS DUPLICATIES*/
+
+// Time complexity O(n) :
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer>sets = new HashSet<>();  
+        
+        for(int i=0; i<nums.length ; i++)
+        {
+             if (sets.contains(nums[i]))  // checks whether the element is in the set
+             {
+                return true;
+             }
+             else
+             {
+                sets.add(nums[i]); // if not, then it adds that element to the set
+             }
+        }
+        return false;    
+    }
+}
