f5fdb6dc82e3dd042c82292c760876435580ba75
chinmaybhat992-create <chinmaybhat992@gmail.com>
2025-09-06 21:03:20 +0530

    Conatain Duplicates(1).java

--- LC_217.java
+++ LC_217.java
@@ -5,7 +5,7 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer>sets = new HashSet<>();  
         
-        for(int i=0; i<nums.length ; i++)
+        for(int i=0; i<nums.length ; i++) 
         {
              if (sets.contains(nums[i]))  // checks whether the element is in the set
              {
